21b108afc36353bf6a4fd7b35951c1cc7676b34a
Mikhail Tyuftyaev <tyuftiaev@mail.ru>
2022-07-17 16:57:46 +0300

    Create middleware

---
+++ src/services/middleware/index.ts
@@ -0,0 +1,44 @@
+import { TwsFeedActions } from "../actions/feed";
+
+const socketMiddleware = (wsActions: TwsFeedActions) => {
+    return (store: any) => {
+        let socket = null;
+
+        return (next: any) => (action: any) => {
+            const {dispatch} = store;
+            const {type, payload} = action;
+            const {
+                wsInit,
+                wsClose,
+                onOpen,
+                onError,
+                onMessage,
+            } = wsActions;
+            if (type === wsInit) {
+                socket = new WebSocket(payload);
+                socket.onopen = (event) => {
+                    dispatch({ type: onOpen, payload: event});
+                };
+
+                socket.onerror = (event) => {
+                    dispatch({ type: onError, payload: event});
+                }
+
+                socket.onmessage = (event) => {
+                    const {data} = event;
+                    const parsedData = JSON.parse(data);
+                    dispatch({
+                        type: onMessage,
+                        payload: {
+                            data: parsedData,
+                            timestamp: new Date().getTime() / 100,
+                        },
+                    });
+                }
+            }
+            next(action);
+        }
+    }
+}
+
+export default socketMiddleware;
